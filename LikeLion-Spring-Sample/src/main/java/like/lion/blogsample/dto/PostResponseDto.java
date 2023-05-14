b6915e95b591870d60040c91ff16d31950dc6e03
pmsu2007 <pmsu2008@gmail.com>
2023-05-14 13:41:13 +0900

    5주차 과제

---
+++ LikeLion-Spring-Sample/src/main/java/like/lion/blogsample/dto/PostResponseDto.java
@@ -0,0 +1,25 @@
+package like.lion.blogsample.dto;
+
+
+import like.lion.blogsample.domain.Post;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class PostResponseDto {
+
+    private Long id;
+    private String title;
+    private String content;
+
+    public PostResponseDto(Post post) {
+        this.id = post.getId();
+        this.title = post.getTitle();
+        this.content = post.getContent();
+    }
+}
