fb0a72c16001b50b6360fc19af94fa4497c1b059
rlagns1234 <tonyk0521@naver.com>
2023-05-16 17:58:38 +0900

    3주차과제

--- springboot-sample-main/springboot-sample-main/src/main/java/com/test/SpringBootApi/domain/NoticeBoard.java
+++ springboot-sample-main/springboot-sample-main/src/main/java/com/test/SpringBootApi/domain/NoticeBoard.java
@@ -1,8 +1,15 @@
 package com.test.SpringBootApi.domain;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import javax.persistence.*;
 
 @Entity
+@Getter
+@Setter
+@NoArgsConstructor
 @Table(name="notice_board")
 public class NoticeBoard {
     @Id
@@ -15,30 +22,30 @@ public class NoticeBoard {
     @Column(name = "content")
     private String content;
 
-    NoticeBoard() {}
+//    NoticeBoard() {}
 
     public NoticeBoard(String title, String content) {
         this.title = title;
         this.content = content;
     }
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getContent() {return content;}
-
-    public void setContent(String content) {this.content = content;}
+//    public Long getId() {
+//        return id;
+//    }
+//
+//    public void setId(Long id) {
+//        this.id = id;
+//    }
+//
+//    public String getTitle() {
+//        return title;
+//    }
+//
+//    public void setTitle(String title) {
+//        this.title = title;
+//    }
+//
+//    public String getContent() {return content;}
+//
+//    public void setContent(String content) {this.content = content;}
 }
--- springboot-sample-main/springboot-sample-main/src/main/java/com/test/SpringBootApi/service/NoticeBoardService.java
+++ springboot-sample-main/springboot-sample-main/src/main/java/com/test/SpringBootApi/service/NoticeBoardService.java
@@ -1,14 +1,23 @@
 package com.test.SpringBootApi.service;
 
 import com.test.SpringBootApi.domain.NoticeBoard;
+import com.test.SpringBootApi.dto.NoticeBoardRequestDto;
+import com.test.SpringBootApi.dto.NoticeBoardReturnDto;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface NoticeBoardService {
 
     public NoticeBoard save(NoticeBoard board);
+//    public Optional<NoticeBoard> findById(Long id);
+//    public NoticeBoard update(Long id, NoticeBoard board);
     public Optional<NoticeBoard> findById(Long id);
-    public NoticeBoard update(Long id, NoticeBoard board);
+    public List<NoticeBoardReturnDto> findAll();
+    public NoticeBoardReturnDto update(Long id, NoticeBoardRequestDto noticeBoardRequestDto);
+
+    NoticeBoard update(Long id, NoticeBoard board);
+
     public void delete(Long id);
 
 }
