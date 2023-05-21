d3cbd6a5248f9c990f380e6d9a68f1ce7d8b3681
Semin Choi <89959944+guitarcode@users.noreply.github.com>
2023-05-21 22:40:19 +0900

    Revert "김동현 (5주차 과제)"

--- 4주차/spring-tutorial/src/main/java/smu/likelion/springtutorial/repository/PostRepository.java
+++
@@ -1,8 +0,0 @@
-package smu.likelion.springtutorial.repository;
-
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import smu.likelion.springtutorial.domain.Post;
-
-public interface PostRepository extends JpaRepository<Post, Long> {
-}
