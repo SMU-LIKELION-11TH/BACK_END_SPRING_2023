b29a2177d610b51630af57c0daca2ecc465e9448
Eunji1217 <94281256+Eunji1217@users.noreply.github.com>
2023-06-03 02:31:21 +0900

    06주차 과제 이은지
    제출합니다~

---
+++ 6주차 과제/LikeLion_Sample_Complete 2/src/main/java/com/test/SpringBootApi/respository/ProductRepository.java
@@ -0,0 +1,13 @@
+package com.test.SpringBootApi.respository;
+
+import com.test.SpringBootApi.domain.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long> {
+    List<Product> findByProductName(String productName);
+}
---
+++ 6주차 과제/LikeLion_Sample_Complete 2/src/main/java/com/test/SpringBootApi/respository/StockInfoRepository.java
@@ -0,0 +1,12 @@
+package com.test.SpringBootApi.respository;
+
+import com.test.SpringBootApi.domain.StockInfo;
+import org.springframework.data.jpa.repository.EntityGraph;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface StockInfoRepository extends JpaRepository<StockInfo, Long> {
+    @EntityGraph(attributePaths = {"store", "product"})
+    List<StockInfo> findAllByStoreId(Long storeId);
+}
